0b5380ee9f93e9ced693e7e8ce3898374a95d3f0
jnguyen897 <64156629+jnguyen897@users.noreply.github.com>
2020-05-01 08:29:11 -0700

    Fix HTML error

--- exercise6/app.js
+++ exercise6/app.js
@@ -1,12 +1,4 @@
 
-var facts = [
-	"40% of Vietnames have the last name 'Nguyen'.", 
-	"The longest flight in the world is Singapore Airlines' SQ21/22 direct flight from Singapore to Newark, New Jersey, is currently the longest flight in the world, lasting around 18 hours and 30 minutes and traveling 9,534 miles.",
-	"Canada is home to over half of the world's natural lakes, with a total of 31,752 lakes.",
-	"North Korea and Cuba are the only places you can't buy Coca-Cola.",
-	"Victoria is home to the first and only diesel-electric hybrid double decker bus to run in North America."
-]; 
-
-function changeDisplay(i) {
-	document.getElementById("output").innerHTML = facts [i];
+function changeDisplay(s) {
+	document.getElementById("output").innerHTML = s;
 }
