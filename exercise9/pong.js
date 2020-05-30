8e1850a786c7f37a564d8d58de2a42df3d3ef1ae
jnguyen897 <64156629+jnguyen897@users.noreply.github.com>
2020-05-30 00:17:14 -0700

    Final Version

--- exercise9/pong.js
+++ exercise9/pong.js
@@ -2,11 +2,16 @@ var speedOfPaddle1 = 0;
 var speedOfPaddle2 = 0;
 var score1 = 1;
 var score2 = 1;
+var heart1 = 1;
+var heart2 = 1; 
 
 var paddleHeight1 = document.getElementById("paddle1").offsetHeight; 
 var paddleHeight2 = document.getElementById("paddle2").offsetHeight; 
 var positionOfPaddle1 = document.getElementById("paddle1").offsetTop; 
 var positionOfPaddle2 = document.getElementById("paddle2").offsetTop; 
+
+const startPositionOfPaddle1 = document.getElementById("paddle1").offsetTop; 
+const startPositionOfPaddle2 = document.getElementById("paddle2").offsetTop;
 
 const ballHeight = document.getElementById("ball").offsetHeight;
 const startTopPositionOfBall = document.getElementById("ball").offsetTop;
@@ -19,16 +24,24 @@ const gameBoardWidth = document.getElementById("gameBoard").offsetWidth;
 
 var topPositionOfBall = startTopPositionOfBall;
 var	leftPositionOfBall = startLeftPositionOfBall; 
-var topSpeedOfBall = 0;
-var leftSpeedOfBall = 0;
+var topSpeedOfBall = 3;
+var leftSpeedOfBall = 3;
+var speedOfBall = 1000/60; 
+
 
 var bounce = new sound ("bounce.mp3"); 
 var exit = new sound ("exit.mp3"); 
 
+// used to control game start/stop
+var controlPlay; 
+
+/*
 window.addEventListener('load', function() {
 	startBall();
-});
+}); */
 
+// move paddles
+// move paddles
 document.addEventListener('keydown', function(e) {
 	// console.log("key down " + e.keyCode); 
 	if (e.keyCode == 87 || e.which == 87) {
@@ -37,7 +50,7 @@ document.addEventListener('keydown', function(e) {
 	
 	if (e.keyCode == 83 || e.which == 83) {
 		speedOfPaddle1 = 10;
-	} // if
+	} // iftyggfgd
 	
 	if (e.keyCode == 38 || e.which == 38) {
 		speedOfPaddle2 = -10;
@@ -46,7 +59,28 @@ document.addEventListener('keydown', function(e) {
 	if (e.keyCode == 40 || e.which == 40) {
 		speedOfPaddle2 = 10;
 	} // if 
-}); 
+	
+	if (e.keyCode == 120 || e.which == 120 || e.KeyCode == 88 || e.which == 88) {
+		if (score1 > 5 && paddleHeight1 == 150) {
+			doubleHeight(3000, 1);
+			score1 -= 5;
+			document.getElementById("score1").innerHTML = score1;
+		} else {
+			window.alert("You need at least 5 points to use this bonus"); 
+		} // if else
+	} // if 
+	
+	if (e.keyCode == 112 || e.which == 112 || e.KeyCode == 80 || e.which == 80) {
+		if (score2 > 5 && paddleHeight2 == 150) {
+			doubleHeight(3000, 2); 
+			score2 -= 5;
+			document.getElementById("score2").innerHTML = score2;
+		} else {
+			window.alert("You need at least 5 points to use this bonus"); 
+		} // else
+	} // if 
+}); // event function 
+
 
 document.addEventListener('keyup', function(e) {
 	// console.log("key up " + e.keyCode); 
@@ -84,7 +118,7 @@ function sound(src) {
   }
 }
 
-function startBall() {
+function startBall(topSpeedOfBall) {
 	let direction = 1;
 	topPositionOfBall = startTopPositionOfBall;
 	leftPositionOfBall = startLeftPositionOfBall;
@@ -95,12 +129,11 @@ function startBall() {
 		direction = -1;
 	} // else
 		
-	topSpeedOfBall = (Math.random() * 2 + 3);
-	leftSpeedOfBall = direction * (Math.random() * 2 + 3); 
+	leftSpeedOfBall = direction * topSpeedOfBall; 
 }
 
 // update the location of paddles and the ball
-window.setInterval (function show() {
+function show() { 
 	positionOfPaddle1 += speedOfPaddle1; 
 	positionOfPaddle2 += speedOfPaddle2;
 	topPositionOfBall += topSpeedOfBall;
@@ -134,8 +167,25 @@ window.setInterval (function show() {
 			document.getElementById("score1").innerHTML = score1++;
 			bounce.play(); 
 		} else {
-			startBall(); 
-			exit.play(); 
+			if (heart1 < 3) {
+				heart1++;
+				hideImage("player1life" + heart1);
+				speedOfBall-=5; 
+				if (speedOfBall < 5) {
+					speedOfBall = 5;
+				} 
+				topSpeedOfBall--;
+				leftSpeedOfBall--; 
+				startBall(topSpeedOfBall, leftSpeedOfBall); 
+				exit.play(); 
+				/* speedOfBall = speedOfBall - 5;
+				if (speedOfBall < 10) {
+					speedOfBall = 10;
+				} // if */ 
+			} else {
+				hideImage("player1life3");
+				stopGame(); 
+			} // else
 		} // else
 	} // if
 
@@ -146,13 +196,153 @@ window.setInterval (function show() {
 			document.getElementById("score2").innerHTML = score2++;
 			bounce.play(); 
 		} else {
-			startBall(); 
-			exit.play(); 
+			if (heart2 < 3) {
+				heart2++; 
+				hideImage("player2life" + heart2); 
+				speedOfBall-=5; 
+				if (speedOfBall < 5) {
+					speedOfBall = 5;
+				} 
+				topSpeedOfBall++;
+				leftSpeedOfBall++; 
+				startBall(topSpeedOfBall, leftSpeedOfBall); 
+				exit.play(); 
+				/* speedOfBall = speedOfBall - 5;
+				if (speedOfBall < 10) {
+					speedOfBall = 10;
+				} // if */ 
+			} else { 
+				stopGame(); 
+				hideImage("player2life3");
+			} // else 
 		} // else
 	}	
-	
 	document.getElementById("paddle1").style.top = positionOfPaddle1 + "px"; 
 	document.getElementById("paddle2").style.top = positionOfPaddle2 + "px"; 
 	document.getElementById("ball").style.top = topPositionOfBall + "px"; 
 	document.getElementById("ball").style.left = leftPositionOfBall + "px"; 
-}, (1000/60)) // show
+	
+} // show
+
+/* function changePaddleHeight(paddle) {
+	if (paddle == 1) {
+		paddleHeight1 = 150;
+		document.getElementById("paddle1").style.height = "150px"; 
+	} else if (paddle == 2) {
+		paddleHeight2 = 150;
+		document.getElementById("paddle2").style.height = "150px"; 
+	} // else if 
+} // changePaddleHeight */ 
+
+function doubleHeight (timeTillReset, paddle) {
+	if (paddle == 1) { 
+		paddleHeight1 *= 2;
+		document.getElementById("paddle1").style.height = paddleHeight1 + "px";
+		setTimeout (function() {
+			paddleHeight1 /= 2;
+			document.getElementById("paddle1").style.height = paddleHeight1 + "px";
+		}, timeTillReset);
+	} else if (paddle == 2) {
+		paddleHeight2 *= 2;
+		document.getElementById("paddle2").style.height = paddleHeight1 + "px";
+		setTimeout (function() {
+			paddleHeight2 /= 2;
+			document.getElementById("paddle2").style.height = paddleHeight1 + "px";
+		}, timeTillReset); 
+	} // else
+} // doubleHeight 
+
+// resume game play
+function resumeGame () {
+	if (!controlPlay) {
+		controlPlay = window.setInterval(show, speedOfBall);
+	} // if
+} // resumeGame
+
+function pauseGame () {
+	window.clearInterval(controlPlay);
+	controlPlay = false; 
+} // pauseGame 
+
+function startGame() {
+	// reset score, ball and paddle location;
+	showImage("player1life1");
+	showImage("player1life2");
+	showImage("player1life3");
+	showImage("player2life1");
+	showImage("player2life2");
+	showImage("player2life3");
+
+	score1 = 1;
+	score2 = 1;
+	positionOfPaddle1 = startPositionOfPaddle1;
+	positionOfPaddle2 = startPositionOfPaddle2;
+
+	startBall(3, 3);  
+	if (!controlPlay) {
+		controlPlay = window.setInterval(show, speedOfBall);
+	}
+} // startGame
+
+// stop game play
+function stopGame() {
+	window.clearInterval(controlPlay);
+	controlPlay = false; 
+	
+	// show lightbox with point
+	let message1 = "Tie Game";
+	let message2 = "Close to continue"; 
+	
+	if (score2 > score1) {
+		message1 = "Player 2 wins with " + (score2-1) + " points";
+		message2 = "Player 1 had " + (score1-1) + " points"; 
+	} else if (score1 > score2) {
+		message1 = "Player 1 wins with " + (score1-1) + " points";
+		message2 = "Player 2 had " + (score2-1) + " points"; 
+	} // else if
+	
+	showLightBox(message1, message2); 
+} // pause game
+
+function hideImage(divID) {
+	var element = document.getElementById(divID);
+	//if element exists, toggle it's class
+	//between hidden and unhidden 
+	if (element.className != 'imgHidden') {
+		element.className = 'imgHidden';
+	} // if
+} // hideImage
+
+function showImage(divID) {
+	var element = document.getElementById(divID);
+	if (element.className != 'imgUnhidden') {
+		element.className = 'imgUnhidden';
+	} // if
+}
+
+/**** Lightbox Code ****/
+function changeVisibility(divID) {
+	var element = document.getElementById(divID);
+	//if element exists, toggle it's class
+	//between hidden and unhidden 
+	if (element) {
+		element.className = (element.className == 'hidden')? 'unhidden' : 'hidden'; 
+	} // if
+} // changeVisibility
+
+function showLightBox(message, message2) {
+	//set message
+	document.getElementById("message").innerHTML = message;
+	document.getElementById("message2").innerHTML = message2;
+	
+	//show lightbox
+	changeVisibility("lightbox");
+	changeVisibility("boundaryMessage");
+} // showLightBox
+
+// closeLightBox
+
+function continueGame() {
+	changeVisibility("lightbox");
+	changeVisibility("boundaryMessage");
+} // continueGame 
